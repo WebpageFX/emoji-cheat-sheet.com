0565f2fe8f40d749af25a6fe4d66657491115631
Niclas Gelin <info@niclasgelin.se>
2012-04-08 00:14:22 +0200

    Restructured scripts
    consolidated and get social scripts async with ga-snippet

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,38 @@
+$(function() {
+  if (FlashDetect.installed) {
+    clip_flash_block_detect = new ZeroClipboard.Client();
+    clip_flash_block_detect.glue(document.getElementById('flash-test'));
+
+    clip = new ZeroClipboard.Client();
+    clip.reposition(document.getElementById('flash-test'));
+    clip.receiveEvent('mouseover', null);
+
+    $.jnotify.setup({ 'delay': 1000, 'fadeSpeed': 500 });
+
+    clip.setHandCursor(true);
+    clip.addEventListener('complete', function(client, text) {
+      $.jnotify('Copied <code>' + text + '</code>');
+      _gaq.push(['_trackEvent', 'Emojis', 'Copy', text]);
+    });
+
+    $('ul.buttons').on('mouseover', 'div', function() {
+      try {
+        if (clip_flash_block_detect.movie.PercentLoaded()){
+          clip.setText($(this).text().trim());
+          if (clip.div && !reglue) {
+            clip.receiveEvent('mouseout', null);
+            clip.reposition(this);
+          } else {
+            clip.glue(this);
+            reglue = false;
+          }
+          clip.receiveEvent('mouseover', null);
+        }
+      } catch(e) { }
+    });
+  }
+});
+
 var accessibility = {
 
   init: function() {
